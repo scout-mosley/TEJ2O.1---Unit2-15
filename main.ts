ca68ae03fc527635c2c65c976c8aed888a050581
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-11-05 08:54:06 -0500

    shows led and has varables

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 205 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Scout Mosley
+ * Created on: Oct 2025
+ * This program uses nested loops for Leds.
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+let insideLoop = 4
+let counter = 4
+led.plot(0, 0)
+
+// does nested loop
+input.onButtonPressed(Button.A, function () { 
+    while (counter > 0) {
+        while (insideLoop > 0) {
+        insideLoop = insideLoop -1
+        
+        }
+     }
+
+
+ })
