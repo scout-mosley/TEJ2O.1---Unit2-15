60236b9d544ee09f2028cfd44e9500fade15f5ee
Scout Mosley <scout.mosley@stu.ocsb.ca>
2025-11-05 09:02:39 -0500

    uses nested loops to sent led around edge of microbit

--- main.ts
+++ main.ts
@@ -9,8 +9,7 @@
 basic.clearScreen()
 let insideLoop = 4
 let counter = 4
-led.plot(0, 0)
-let sprite: game.LedSprite = null
+let sprite = game.createSprite(0, 0)
 
 // does nested loop
 input.onButtonPressed(Button.A, function () { 
@@ -18,9 +17,13 @@ input.onButtonPressed(Button.A, function () {
         while (insideLoop > 0) {
         insideLoop = insideLoop -1
         sprite.move(1) 
+        basic.pause(500)
+
         }
-        
+        insideLoop = insideLoop +4
+        counter = counter -1
+        sprite.turn(Direction.Right, 90)
      }
 
-
  })
+ 
